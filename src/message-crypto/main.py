146bc67b02053b8522ddcc09d2ed5ca31fee09b1
Kira <kiratechno@pm.me>
2020-12-17 17:20:06 +0300

    Added commentary to main.py

--- src/message-crypto/main.py
+++ src/message-crypto/main.py
@@ -2,3 +2,39 @@ import pki
 
 encrypt = pki.Encrypt.Encrypt()
 decrypt = pki.Decrypt.Decrypt(input('Enter your passphrase: '))
+keys = pki.KeyManager.KeyManager()
+
+# we need to run telegram here
+
+
+# upon receiving 
+
+# '-----BEGIN PGP PUBLIC KEY BLOCK' in a message
+
+# we add a (user_id, user_key_id) to our database
+
+# and do keys.import_key(keydata)
+
+
+# upon receiving
+
+# '-----BEGIN PGP MESSAGE' in a message
+
+# we pass it to decrypt.decrypt_string(msg)
+
+# and display plain text
+
+
+# if we want to send a message to user with username
+
+# we need to first resolve the username into a
+
+# telegram user_id and then look up this user's public key
+
+# in our database
+
+# if key entry exists, we pass the plain text message
+
+# to encrypt.encrypt(key_id, msg) and then send 
+
+# the armored output over telegram
