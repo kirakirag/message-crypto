6aed83419cf9d123e8b6b992f041b3773eb87748
Catz <catnyan2002@gmail.com>
2020-12-17 21:55:54 +0300

    Client. Gets dialogues. Can choose one. Prints messages from this dialogue. Parses new messages in this chat.

--- src/message-crypto/t_client.py
+++ src/message-crypto/t_client.py
@@ -1,58 +1,41 @@
-from telethon import TelegramClient, events, sync
+import asyncio
+
+from telethon import TelegramClient, events
+
+# Remember to use your own values from my.telegram.org!
+api_id = 'some_api_id'
+api_hash = 'some_api_hash'
+client = TelegramClient('anon', api_id, api_hash)
+dialog_num = 0
 
 
-api_id = 2291221
-api_hash = 'f788a63b55201ecf2856e33922e777d1'
+@client.on(events.NewMessage())
+async def my_event_handler(event):
+    chat = await event.get_chat()
+    global dialog_num
+    if chat.id == dialog_num:
+        sender = await event.get_sender()
+        print(sender.username, event.date, event.text)
 
 
-class Session:
+async def main():
 
-    def __init__(self):
-        """
-        Initialization of a new session.
-        """
-        self.client = TelegramClient('user', api_id, api_hash)
+    me = await client.get_me()
+    print(me.id)
 
-    def start_session(self):
-        """
-        Start new session.
-        """
-        self.client.start()
-        pass
+    async for dialog in client.iter_dialogs():
+        print(dialog.name, dialog.id)
 
-    def get_fingerprint(self):
-        """
-        Get fingerprint for user.
-        """
-        return self.client.get_me().id
+    global dialog_num
+    dialog_num = int(input('Введите ID чата '))
 
-    def get_dialogs(self):
-        """
-        Get user's dialogs.
-        """
-        return self.client.get_dialogs()
-
-    def stop_session(self):
-        """
-        Stop this session.
-        """
-        self.client.disconnect()
-
-    def dialogs_for_print(self):
-        dialog_list = ['{}    {}'.format(dialog[0], dialog[1].name) for dialog in enumerate(self.get_dialogs())]
-        return dialog_list
-
-    def chat_for_print(self):
-        chat_number = int(input('Number of the chat you want to open '))
-        message_list = []
-        for message in self.client.get_messages(self.get_dialogs()[chat_number].entity, 20):
-            message_list.append('From {} at {}: {}'.format(message.get_sender().username,
-                                                           message.date, message.message))
-        return message_list
+    async for message in client.iter_messages(dialog_num):
+        sender = await message.get_sender()
+        print(sender.username, message.date, message.text)
 
 
-c = Session()
-c.start_session()
-print(*c.dialogs_for_print(), sep='\n')
-print(*c.chat_for_print(), sep='\n')
-c.stop_session()
+with client:
+    client.loop.run_until_complete(main())
+    print(dialog_num)
+    client.start()
+    client.run_until_disconnected()
