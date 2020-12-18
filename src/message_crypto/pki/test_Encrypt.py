c9dd255dc76c5604224f0419819c0339d2bd28af
Catz <catnyan2002@gmail.com>
2020-12-18 13:02:35 +0300

    More tests

--- src/message_crypto/pki/test_Encrypt.py
+++ src/message_crypto/pki/test_Encrypt.py
@@ -15,6 +15,15 @@ class EncryptTest(unittest.TestCase):
     def test_string_with_numbers(self):
         self.assertEqual(decryption.decrypt_string(encryption.encrypt_string('4FC94BFCCB82491E', 'a8890012bacsdas9882131823--0--0')), 'a8890012bacsdas9882131823--0--0')
 
+    def test_string_with_spaces(self):
+        self.assertEqual(decryption.decrypt_string(encryption.encrypt_string('4FC94BFCCB82491E', 'huh hello nice')), 'huh hello nice')
+
+    def test_string_with_punctuation(self):
+        self.assertEqual(decryption.decrypt_string(encryption.encrypt_string('4FC94BFCCB82491E', 'hello!!!! how are you?')), 'hello!!!! how are you?')
+
+    def test_string_with_capitals(self):
+        self.assertEqual(decryption.decrypt_string(encryption.encrypt_string('4FC94BFCCB82491E', 'HELLO')), 'HELLO')
+
     def test_nonexistent_keyid(self):
         self.assertFalse(decryption.decrypt_string(encryption.encrypt_string('1', 'a8890012bacsdas9882131823--0--0')), 'a8890012bacsdas9882131823--0--0')
         
